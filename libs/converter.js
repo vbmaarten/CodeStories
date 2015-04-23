f672c8d9ee68ff110288681012319228c4f53862
Maarten van Beek <maartenvanbeek@gmail.com>
2015-04-23 14:16:45 +0200

    Merged

--- libs/converter.js
+++ libs/converter.js
@@ -1,8 +1,29 @@
+function convertArray(object){
+	result = []
+	for(property in object.properties){
+		result[property] = convertProperty(object.properties[property]);
+	}
+}
+
+function convertObject(object){
+	if(object.length){
+		return convertArray(object);
+	} else{
+		result = {};
+		for(property in object.properties){
+			obj[property] = convertProperty(object.properties[property]);
+		}
+		return result;
+	}
+}
+
 function convertProperty(property){
 	if(property.isPrimitive){
 		return property.data;
 	} else {
-		return null;
+		if(property.type === 'object'){
+			return convertObject(property);
+		}
 	}
 }
 
@@ -10,12 +31,12 @@ function convertScope(interpreter_scope){
 	var  scope;
 
 	if(interpreter_scope.parentScope){
-		scope = convertScope(interpreter_scope.parentScope);
+		scope = convertScope(scope.parentScope);
 	} else{
 		return {};
 	}
 	for(property in interpreter_scope.properties){
-		scope[property] = convertProperty(interpreter_scope.properties[property]);
+		scope[property] = convertProperty(scope.properties[property]);
 	}
 	
 	return scope;
