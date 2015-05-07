dbcb6254ab4f37189ac5cbf50481d0dcd04891f7
Maarten van Beek <maartenvanbeek@gmail.com>
2015-05-07 13:49:07 +0200

    Added zip->cast code and necesarry libraries

--- src/app/projectLoader/projectParser/projectParser.factory.js
+++ src/app/projectLoader/projectParser/projectParser.factory.js
@@ -1,18 +1,32 @@
-'use strict';
+"use strict";
 
-/**
- * @ngdoc function
- * @name projectLoader.factory:projectParser
- * @description
- * # projectParser
- * Factory for parsing a project into an CAST
- */
-angular.module('projectLoader')
-  .factory('projectParser', ['castFactory', function (castFactory) {
+angular.module("projectLoader").factory("projectParser", [ "castFactory", function(castFactory) {
     return {
-    	parse: function (code) {
-    		
-    		castFactory.setCast(code);
-    	}
-    }
-  }]);
+        loadZip: function(data) {
+            var zip = new JSZip(data);
+            files = Object.keys(zip.files);
+            for (var file_i = 0; file_i < files.length; file_i++) {
+                file = zip.files[files[file_i]];
+                path = file.name.split("/");
+                save_root = this.root;
+                for (var i = 0; i < path.length; i++) {
+                    if (i == path.length - 1) {
+                        if (path[i] != "") {
+                            extension = path[i].split(".");
+                            extension = extension[extension.length - 1];
+                            content = zip.file(file.name).asBinary();
+                            save_root[path[i]] = {
+                                content: content
+                            };
+                        }
+                    } else {
+                        if (!save_root.hasOwnProperty(path[i] + "/")) {
+                            save_root[path[i] + "/"] = {};
+                        }
+                        save_root = save_root[path[i] + "/"];
+                    }
+                }
+            }
+        }
+    };
+} ]);
