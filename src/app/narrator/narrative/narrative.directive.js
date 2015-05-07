511e04eba6a3c7b4b1235539e7eb6466e6ca7baf
RoelofSol <rs@wlan-145-94-190-7.wlan.tudelft.nl>
2015-05-07 13:08:15 +0200

    some basic narrative controls

--- src/app/narrator/narrative/narrative.directive.js
+++ src/app/narrator/narrative/narrative.directive.js
@@ -8,6 +8,28 @@
  * Diretive of the narrative
  */
 angular.module('narrator')
-  .directive('narrative', function ($scope) {
-    
+  .directive('narrative', function () {
+    return {
+    	templateUrl:'narrator/narrative/narrative.html',
+    }
+  }).directive('primitive', function () {
+    return {
+    	templateUrl:'narrator/narrative/primitives/primitive.html',
+    }
+  }).directive('addNarrativeBtn',function(){
+  	return {
+  		template:  "<div ng-show='writerMode' ng-click='addNarrative(storyBoard,$index);'>+</div>",	
+  	}
+  }).directive('removeNarrativeBtn',function(){
+  	return {
+  		template:  "<div ng-show='writerMode' ng-click='removeNarrative(storyBoard,$index);'>-</div>",	
+  	}
+  }).directive('addPrimitiveBtn',function(){
+  	return {
+  		template:  "<div ng-show='writerMode' ng-click='addPrimitive(narrative,$index,atindex);'>+</div>",	
+  	};
+  }).directive('removePrimitiveBtn',function(){
+ 	return {
+  		template:  "<div ng-show='writerMode' ng-click='removePrimitive(narrative,$index);'>-</div>",	
+  	};
   });
--- src/app/narrator/narrator.controller.js
+++ src/app/narrator/narrator.controller.js
@@ -8,39 +8,60 @@
  * Controller of the narrator
  */
 
- var activeNarative = [
-
+ var nar1 =
  {'path':"/something/",
  'primitives':[
  {"text":"<p> somet text </p>"},
- {"text":"<p> more text </p>"}
- ]},
+ {"text":"<p> more text </p>"},
+ ]}
+
+ var nar2 = 
  {'path':"/someotherfile.js/",
  'primitives':[
  {"text":"<p> this is about otherfile.js</p>"},
  {"text":"<p> and so is this </p>"}
  ]}
- ]
+ nar1.primitives.push({'link':nar2});
 
 
- angular.module('narrator')
+ angular.module('narrator',["xeditable"])
  .controller('NarratorCtrl', function ($scope) {
 
- 	$scope.writer = false;
+ 	$scope.writerMode = true	;
 
- 	// All the steps that have been executed so far
- 	$scope.stepped = activeNarative
- 	$scope.currentNaratives
-
+ 	$scope.storyBoard = []
+ 	$scope.activeNarrative = {'path':'/','primitives':[{'link':nar1}]};
+ 	$scope.primitiveIndex = 0
  	
-
- 	function next(){
-
-
-
+ 	$scope.next =function (){
+ 		
+ 		var step = $scope.activeNarrative.primitives[$scope.primitiveIndex];
+ 		if(step.link){
+ 			$scope.storyBoard.push( 
+ 				{'path':step.link.path ,'primitives':[]}
+ 				)
+ 			$scope.activeNarrative = step.link
+ 			$scope.primitiveIndex=0;
+ 		}
+ 		else {
+ 			console.log($scope)
+ 			$scope.storyBoard[$scope.storyBoard.length-1].primitives.push(
+ 				$scope.activeNarrative.primitives[$scope.primitiveIndex]
+ 				)
+ 			$scope.primitiveIndex++;
+ 		}
  	}
 
+    $scope.addNarrative = function(storyBoard,index){
+ 		index = index ;
+ 		storyBoard.splice(index+1,0,{"path":"/todo/",'primitives':[]});
+ 	};
+ 	$scope.removeNarrative = function(storyBoard,index){
+ 		storyBoard.splice(index,1);
 
+ 	};
+
+ 	
 
 
 
