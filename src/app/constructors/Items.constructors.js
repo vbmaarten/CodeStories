e158635673cc8c102973199d433cd48a4600bd54
Maarten van Beek <maartenvanbeek@gmail.com>
2015-05-12 17:02:33 +0200

    Added item types

--- src/app/constructors/Items.constructors.js
+++ src/app/constructors/Items.constructors.js
@@ -1,1 +1,28 @@
-Narrative.constructors.js
+var Item = function(content){
+	this.content = content;
+}
+
+var TextItem = function (content) {
+	Item.call(this,content);
+};
+TextItem.prototype = Object.create(Item.prototype);
+
+var VideoItem = function (content) {
+	Item.call(this,content);
+};
+VideoItem.prototype = Object.create(Item.prototype);
+
+var PictureItem = function (content) {
+	Item.call(this,content);
+};
+PictureItem.prototype = Object.create(Item.prototype);
+
+var VCodeItem = function (content) {
+	Item.call(this,content);
+};
+VCodeItem.prototype = Object.create(Item.prototype);
+
+var LinkItem = function (content) {
+	Item.call(this,content);
+};
+LinkItem.prototype = Object.create(Item.prototype);
