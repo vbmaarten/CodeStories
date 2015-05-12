83be0ef095ed698f957892ac05e2ca9a834d2667
RoelofSol <rs@wlan-145-94-191-120.wlan.tudelft.nl>
2015-05-12 16:29:19 +0200

    common node functions

--- src/app/constructors/CASTNode.constructors.js
+++ src/app/constructors/CASTNode.constructors.js
@@ -2,6 +2,32 @@ var CASTNode = function(name, parent, children){
 	this.name = name;
 	this.parent = parent;
 	this.children = children;
+	this.getType = function(){
+		if(this instanceof FolderNode)
+			return 'folder';
+		else if(this instanceof FileNode)
+			return 'file';
+		else if(this instanceof ASTNode)
+			return 'ast_node';
+		throw 'BadNodeTypeError';
+	}
+	this.isFolder = function(){
+		return (this.getType() === 'folder')
+	}
+	this.isFile = function(){
+		return (this.getType() === 'file')
+	}
+	this.isASTNode = function(){
+		return (this.getType() === 'ast_node')
+	}
+	this.up = function(){
+		if(!this.parent){
+			console.error('This node has no parent');
+			throw 'NoParrentError'
+			return this.parent;
+		}
+		return this.parent;
+	}
 }
 
 var FolderNode = function (name, parent, children) {
