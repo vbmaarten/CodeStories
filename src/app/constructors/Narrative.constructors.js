444fa4b7b5d8b55d205734c267fb347b8785b625
RoelofSol <rs@wlan-145-94-191-120.wlan.tudelft.nl>
2015-05-12 17:04:41 +0200

    Narrative constructors

--- src/app/constructors/Narrative.constructors.js
+++ src/app/constructors/Narrative.constructors.js
@@ -1,1 +1,68 @@
-Narrative.constructors.js
+var Narrative = function(name, CASTNode){
+	this.name = name;
+	this.parent = parent;
+	this.children = children;
+	this.getType = function(){
+		if(this instanceof FSNarrative)
+			return 'fs_narrative';
+		else if(this instanceof CodeNarrative)
+			return 'code_narrative';
+		throw 'BadNarrativeTypeError';
+	}
+	this.isCodeNarrative = function(){
+		return (this.getType() === 'code_narrative')
+	}
+	this.isFSNarrative = function(){
+		return (this.getType() === 'fs_narrative')
+	}
+	this.removeItem = function(index){
+		if( index instanceof Item)
+			index = this.Items.indexOf(index);
+
+		this.Items.splice(index,1);
+	}
+	this.validItem = function(){
+		return false;
+	}
+	this.addItem = function(item,index){
+		if(!this.validItem(item)){
+			console.error('Trying to add a wrong time of item',item,this);
+			throw "BadItemForNarrative"
+		}
+		//TODO check index
+		this.Items.splice(index,0,item);
+	}
+}
+
+var FSNarrative = function (name, CASTNode) {
+	if( ! (CASTNode.isFile() || CASTNode.isFolder()) ) {
+		console.log(' You can not add a FSNarrative on',CASTNode);
+		throw "BadNarrativeForCASTNode";
+	}
+
+	Narrative.call(this,CASTNode);
+	this.validItem = function(item){
+		return item instanceof Item;
+	}
+	this.Items = new Items();
+};
+
+FSNarrative.prototype = Object.create(CASTNode.prototype);
+
+var CodeNarrative = function (name, CASTNode) {
+	if( !CASTNode.isCodeNarrative() ) {
+		console.log(' You can not add a CodeNarrative on',CASTNode);
+		throw "BadNarrativeForCASTNode";
+	}
+
+	Narrative.call(this,CASTNode);
+	this.validItem = function(item){
+		if(item instanceof LinkItem)
+			return false;
+		return item instanceof Item;
+	}
+	this.Items = [];
+
+};
+
+CodeNarrative.prototype = Object.create(CASTNode.prototype);
