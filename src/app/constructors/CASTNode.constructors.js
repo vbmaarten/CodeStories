047845ae8c34f1603276a0984b27fd6acfaf3831
SalimSalmi <salmi0707@gmail.com>
2015-05-12 16:15:14 +0200

    basic constructor format

--- src/app/constructors/CASTNode.constructors.js
+++ src/app/constructors/CASTNode.constructors.js
@@ -1,1 +1,23 @@
-CAST.js
+var CASTNode = function(name, parent, children){
+	this.name = name;
+	this.parent = parent;
+	this.children = children;
+}
+
+var FolderNode = function (name, parent, children) {
+	CASTNode.call(this,name,parent,children);
+};
+
+FolderNode.prototype = Object.create(CASTNode.prototype);
+
+FolderNode.prototype.type = 'directory';
+
+
+/*
+var node = new FolderNode('test','parent','child');
+
+console.log(node.type); //directory
+console.log(node instanceof CASTNode); //true
+console.log(node instanceof FolderNode); //true
+
+*/
