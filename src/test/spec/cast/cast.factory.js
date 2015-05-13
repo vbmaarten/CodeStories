3e9626ee5c6586438c589074ba7a2fa79fb293ad
SalimSalmi <salmi0707@gmail.com>
2015-05-13 10:52:15 +0200

    unittest test file, should pass grunt test

---
+++ src/test/spec/cast/cast.factory.js
@@ -0,0 +1,21 @@
+describe('cast', function () {
+
+  it('test', function () {
+    expect(true).toBe(true);
+  });
+/*
+  var Person;
+  beforeEach(module('myApp'));
+  beforeEach(inject(function (_Person_) {
+    Person = _Person_;
+  }));
+
+  describe('Constructor', function () {
+
+    it('assigns a name', function () {
+      expect(new Person('Ben')).to.have.property('name', 'Ben');
+    });
+
+  });*/
+
+});
