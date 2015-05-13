1c7f37cbde927016146e387b51b4dba5e0a895f3
RoelofSol <rs@wlan-145-94-189-63.wlan.tudelft.nl>
2015-05-13 11:56:29 +0200

    Narrator and item tests setup

--- src/test/mock/CastMock.js
+++ src/test/mock/CastMock.js
@@ -1,15 +1,10 @@
-'use strict';
 
-/**
- * @ngdoc function
- * @name cast.factory:CAST
- * @description
- * # CAST
- * Factory of the cast
- */
+var filesystemMock = {}
+
+var narrativesJsonMock = {}
 
 
- var mock = {
+ var CastMock = {
   '/': {
     "name": "/",
     "type": "directory",
@@ -95,30 +90,3 @@
     }
   }
 }
-
-  angular.module('cast')
-  .factory('CAST', function () {
-    return  { 
-      selected: undefined,
-
-
-
-      getNode: function(p) {
-        var path = p.split('/').join('/,').split(',');
-        
-        if(path[path.length-1] == "")
-          path.pop();
-
-        var current = this.cast['/'];
-        for (var i = 1; i < path.length; i ++){
-          current = current.children[path[i]];
-        }
-        return current;
-      },
-      cast: mock
-    }
-  });
-
-
-
-
