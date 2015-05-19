3e955367eb80231de3e57df98a4f146d49ba13c2
RoelofSol <rs@wlan-145-94-189-140.wlan.tudelft.nl>
2015-05-19 11:58:25 +0200

    more jshint fixes

--- src/app/constructors/Items.constructors.js
+++ src/app/constructors/Items.constructors.js
@@ -1,48 +1,34 @@
-"use strict";
-
-var Item = function(content){
+'use strict';
+var Item = function (content) {
 	this.content = content;
-}
-Item.prototype.buildNewItem = function(item){
+};
+Item.prototype.buildNewItem = function (item) {
 	var type = item.type;
-
-	if(type === "text"){
-		
-			return new TextItem(item.content);
-		}
-	else if(type === "link"){
+	if (type === 'text') {
+		return new TextItem(item.content);
+	} else if (type === 'link') {
 		return new LinkItem(item.content);
-
 	}
-	
-	console.error('unknown item type',item);
+	console.error('unknown item type', item);
 	throw 'unidentifiedItem';
-
-	
-
-}
-
+};
 var TextItem = function (content) {
-	Item.call(this,content);
+	Item.call(this, content);
 };
 TextItem.prototype = Object.create(Item.prototype);
-
 var VideoItem = function (content) {
-	Item.call(this,content);
+	Item.call(this, content);
 };
 VideoItem.prototype = Object.create(Item.prototype);
-
 var PictureItem = function (content) {
-	Item.call(this,content);
+	Item.call(this, content);
 };
 PictureItem.prototype = Object.create(Item.prototype);
-
 var VCodeItem = function (content) {
-	Item.call(this,content);
+	Item.call(this, content);
 };
 VCodeItem.prototype = Object.create(Item.prototype);
-
 var LinkItem = function (content) {
-	Item.call(this,content);
+	Item.call(this, content);
 };
 LinkItem.prototype = Object.create(Item.prototype);
--- src/app/narrator/narrator.controller.js
+++ src/app/narrator/narrator.controller.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name narrator.controller:NarratorCtrl
@@ -7,73 +6,67 @@
  * # NarratorCtrl
  * Controller of the narrator
  */
-
-angular.module('narrator')
-  .controller('NarratorCtrl', [ '$scope', '$stateParams', '$state', 'CAST', 'narratorFactory', 
-  	function ($scope, $stateParams, $state, CAST, narratorFactory) {
-
- 	// Get the current active node in the CAST
- 	if($stateParams.path)
-    $scope.activeNode = CAST.getNode($stateParams.path);
-  else
-   	$scope.activeNode = CAST.getNode('/');
-
-  console.log($stateParams.path);
-
-  // Get the narratives of the current node
-  $scope.narratives = narratorFactory.narratives = $scope.activeNode.narratives;
-
-  // If the user is able to edit the narratives or not (boolean)
- 	$scope.writerMode = narratorFactory.writerMode;
-
-  console.log($scope.activeNode);
-  console.log($scope.narratives);
-
- 	// Navigate to corresponding state
-  if(narratorFactory.writerMode) {
-  	$state.go('narrating.writer');
-  	$scope.state = "Viewer";
-  }
-  else {
-  	$state.go('narrating.viewer');
-  	$scope.state = "Writer";
-  }
-
-  // Function to switch between states
-  $scope.switchMode = function(){
-  	if(narratorFactory.writerMode){
-  		narratorFactory.writerMode = false;
-  		$scope.writerMode = narratorFactory.writerMode;
-  		$scope.state="Writer";
-	  	$state.go('narrating.viewer');
-  	}
-	  else{
-	  	narratorFactory.writerMode = true;
-  		$scope.writerMode = narratorFactory.writerMode;
-  		$scope.state="Viewer"
-	  	$state.go('narrating.writer');
-	  }
-  }
-
-
-  // If there was a narrative linked, continue that narrative
-  if(narratorFactory.narrativeLink) {
-    console.log(narratorFactory.narrativeLink);
-    if(!(narratorFactory.narrativeLink === true)){
-      var linked;
-      for(var index in $scope.narratives) {
-        if($scope.narratives[index].name == narratorFactory.narrativeLink)
-          linked = $scope.narratives[index];
-      }
-      narratorFactory.pushNarrative(linked);
+ angular.module('narrator').controller('NarratorCtrl', [
+  '$scope',
+  '$stateParams',
+  '$state',
+  'CAST',
+  'narratorFactory',
+  function ($scope, $stateParams, $state, CAST, narratorFactory) {
+    // Get the current active node in the CAST
+    if ($stateParams.path){
+      $scope.activeNode = CAST.getNode($stateParams.path);
     }
-    narratorFactory.narrativeLink = undefined;
-    $scope.playing = true;
-  } else {
-    $scope.selected = false;
-    $scope.selectedNarrative = {};
-    $scope.playing = false;
+    else{
+      $scope.activeNode = CAST.getNode('/');
+    }
+    console.log($stateParams.path);
+    // Get the narratives of the current node
+    $scope.narratives = narratorFactory.narratives = $scope.activeNode.narratives;
+    // If the user is able to edit the narratives or not (boolean)
+    $scope.writerMode = narratorFactory.writerMode;
+    console.log($scope.activeNode);
+    console.log($scope.narratives);
+    // Navigate to corresponding state
+    if (narratorFactory.writerMode) {
+      $state.go('narrating.writer');
+      $scope.state = 'Viewer';
+    } else {
+      $state.go('narrating.viewer');
+      $scope.state = 'Writer';
+    }
+    // Function to switch between states
+    $scope.switchMode = function () {
+      if (narratorFactory.writerMode) {
+        narratorFactory.writerMode = false;
+        $scope.writerMode = narratorFactory.writerMode;
+        $scope.state = 'Writer';
+        $state.go('narrating.viewer');
+      } else {
+        narratorFactory.writerMode = true;
+        $scope.writerMode = narratorFactory.writerMode;
+        $scope.state = 'Viewer';
+        $state.go('narrating.writer');
+      }
+    };
+    // If there was a narrative linked, continue that narrative
+    if (narratorFactory.narrativeLink) {
+      console.log(narratorFactory.narrativeLink);
+      if (!(narratorFactory.narrativeLink === true)) {
+        var linked;
+        for (var index in $scope.narratives) {
+          if ($scope.narratives[index].name === narratorFactory.narrativeLink){
+            linked = $scope.narratives[index];
+          }
+        }
+        narratorFactory.pushNarrative(linked);
+      }
+      narratorFactory.narrativeLink = undefined;
+      $scope.playing = true;
+    } else {
+      $scope.selected = false;
+      $scope.selectedNarrative = {};
+      $scope.playing = false;
+    }
   }
-  
-
-}]);
+  ]);
