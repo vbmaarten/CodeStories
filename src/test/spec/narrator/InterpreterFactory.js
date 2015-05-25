af0862151d0c832644294e6d7da6c38e1abea84a
Maarten van Beek <maartenvanbeek@gmail.com>
2015-05-26 00:25:01 +0200

    InterpreterFactory, one test failing due to an error, we have to discuss it

--- src/test/spec/narrator/InterpreterFactory.js
+++ src/test/spec/narrator/InterpreterFactory.js
@@ -1,29 +1,55 @@
 'use strict';
 
 describe('Factory: Interpreter factory', function() {
+  // ---- Initialization ----
   beforeEach(module('codeStoriesApp'));
   beforeEach(module("narrator"));
 
-  var projectLoaderFactory;
+  var interpreterFactory;
   var script = "var sum = 0;\
 \
   for(var i = 0; i < 10; i++){\
   	sum += i;\
   }";
 
-  var astNode = new ASTNode("program", null, {}, acorn.parse(script));
-
-  
+  var astNode = new ASTNode("/", null, {}, acorn.parse(script));
 
   beforeEach(inject(function (_interpreterFactory_){
-    projectLoaderFactory = _interpreterFactory_;
+    interpreterFactory = _interpreterFactory_;
   }));
 
-  tests();
-});
-
-function tests(){
-  it("should", function(){
-  	expect(true).toBe(true);
+  // ---- Tests -----
+  it("should exist", function(){
+  	expect(interpreterFactory).toBeDefined();
   });
-}
+
+  it("should be possible to give it a self-compiled AST", function(){
+    var codeNarrative = new CodeNarrative("codeNarrative", "/", [])
+    interpreterFactory.setupNarratedAST(astNode, codeNarrative);
+
+    expect(interpreterFactory.interpreter.ast.body[0].type).toEqual("VariableDeclaration");
+  });
+
+  it("should be possible to step through it like a debugger", function(){
+    var codeNarrative = new CodeNarrative("codeNarrative", "/", [])
+    interpreterFactory.setupNarratedAST(astNode, codeNarrative);
+
+    expect(interpreterFactory.interpreter.stateStack[0].node.type).toEqual("Program");
+    interpreterFactory.debugStep();    
+    expect(interpreterFactory.interpreter.stateStack[0].node.type).toEqual("VariableDeclaration");
+  });
+
+  it("should be possible to step to the next narrative", function(){
+    var node = astNode.tnode.body[1];
+
+    var codeNarrative = new CodeNarrative("codeNarrative", "/", [{node: "body/0", items: [new TextItem("textitem")]}]);
+    console.log(codeNarrative);
+    interpreterFactory.setupNarratedAST(astNode, codeNarrative);
+
+    //interpreterFactory.narrativeStep();
+
+    expect(interpreterFactory.interpreter.stateStack[0]).toEqual(node);
+  });
+
+
+});
