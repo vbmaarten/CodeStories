2f66faffbf2a444f865cc9bcf29c6fee6fa06e4c
RoelofSol <rs@wlan-145-94-191-45.wlan.tudelft.nl>
2015-05-26 13:19:17 +0200

    cast factory doc

--- src/app/cast/cast.factory.js
+++ src/app/cast/cast.factory.js
@@ -1,11 +1,15 @@
 'use strict';
 
 /**
- * @ngdoc function
+ * @ngdoc service
  * @name cast.factory:CAST
  * @description
- * # CAST
- * Factory of the cast
+ *
+ * Context and Abstract Syntax Tree.  The central datastructue of CodeStories. 
+ It exposes a general path scheme for folder files and AST (of javascript files). 
+ The project loader will setup this tree of CASTNodes and append Narrative data at the propper CASTNode. 
+ The explorer allows a user to browse through the content , and the narrator will display the narratives of a 
+ selected node in the CAST tree.
  */
 
 
@@ -18,20 +22,64 @@ angular.module('cast')
         };
 
         return {
-            // CAST object
+             /**
+           * @ngdoc property
+           * @name cast
+           * @propertyOf cast.factory:CAST
+           * @description
+           * contains the rootnode of the CAST. 
+           */ 
             cast: EmptyCast,
-            // Currently selected node in the CAST
+                         /**
+           * @ngdoc property
+           * @name selected
+           * @propertyOf cast.factory:CAST
+           * @description
+           * Currently selected node in the CAST
+           */ 
+        
             selected: EmptyCast.rootnode,
-            // Path to the currently selected node
+
+                  /**
+           * @ngdoc property
+           * @name selectedPath
+           * @propertyOf cast.factory:CAST
+           * @description
+           * Path to the currently selected node
+           */             
+            
             selectedPath: '/',
-            // File content corresponding to the current node
+
+        
             content: '',
-            // Root url
+
+                              /**
+           * @ngdoc property
+           * @name project
+           * @propertyOf cast.factory:CAST
+           * @description
+           * Root url
+           */ 
+            
             project: '',
-            //narrative list
+
+                              /**
+           * @ngdoc property
+           * @name narratives
+           * @propertyOf cast.factory:CAST
+           * @description
+           * narrative list
+           */ 
+            
             narratives: {},
 
-            // Appends narratives 
+      /**
+       * @ngdoc method
+       * @name appendNarrative
+       * @methodOf cast.factory:CAST
+       * @description
+       * Takes narrative json and appends it to their CASTNodes when loading a project
+       */ 
             appendNarrative: function(narratives) {
                 var i, newNarrative, name;
                 for (var castPath in narratives) {
@@ -54,6 +102,13 @@ angular.module('cast')
             },
             
 
+        /**
+       * @ngdoc method
+       * @name setSelected
+       * @methodOf cast.factory:CAST
+       * @description
+       * Update the selected node. Will probably be replaced by State 
+       */ 
             setSelected:function(node){
               if (typeof node === 'string'){
                 this.selected = this.getNode(node);
@@ -64,7 +119,13 @@ angular.module('cast')
                this.selectedPath = this.selected.getPath();
             },
 
-            // Return node that corresponds to the given path
+             /**
+       * @ngdoc method
+       * @name getNode
+       * @methodOf cast.factory:CAST
+       * @description
+       * takes a path and returns a node
+       */ 
             getNode: function(path) {
                 return this.cast.rootnode.getNode(path);
             },
@@ -73,11 +134,25 @@ angular.module('cast')
               return  this.narratives[this.selectedPath] || [];
             },
 
+             /**
+               * @ngdoc method
+               * @name getNarrative
+               * @methodOf cast.factory:CAST
+               * @description
+               * gets a specific narrative at a path 
+               */ 
             getNarrative:function(path,id){
                 return this.narratives[path][id];
 
             },
 
+             /**
+       * @ngdoc method
+       * @name getNarratives
+       * @methodOf cast.factory:CAST
+       * @description
+       * get all narratives at a path
+       */ 
             getNarratives: function(path) {
                 return this.narratives[path];
             }
