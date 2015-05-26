c103b3a3a491b89bc89e7bbb1d1262569d17fd40
SalimSalmi <salmi0707@gmail.com>
2015-05-26 14:33:39 +0200

    documentation narrator factory

--- src/app/narrator/narrator.factory.js
+++ src/app/narrator/narrator.factory.js
@@ -4,7 +4,7 @@
  * @ngdoc service
  * @name narrator.factory:narratorFactory
  * @requires cast.factory:CAST
- * @requires interpreterFactory
+ * @requires narrator.factory:interpreterFactory
  * @description
  *
  * Factory of the narrator, containers logic of the narrator like selecting, deselecting
