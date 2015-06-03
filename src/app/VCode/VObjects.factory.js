6f263651e51bfcb436d850f4e70a27ef1d462ea4
RoelofSol <rs@wlan-145-94-147-39.wlan.tudelft.nl>
2015-06-03 16:11:04 +0200

    Object update

--- src/app/VCode/VObjects.factory.js
+++ src/app/VCode/VObjects.factory.js
@@ -91,62 +91,51 @@ function BarChart(data){
 	var domEl = document.createElement('div')
 	var chart = d3.select(domEl).append("svg");
 
-	var data;
-
-
-	var barChart;
 	
 
-	function update(newData) {
+	
 
-		data = newData || data;
-		console.log(data);
-		var height = 150 ||   parseInt(domEl.clientHeight) , width = 300 || parseInt(domEl.clientWidth) ;
-
-
-
-		var yScale = d3.scale.linear()
+	var height = 150 ||   parseInt(domEl.clientHeight) , width = 300 || parseInt(domEl.clientWidth) ;
+	
+	var barWidth = width / data.length;
+	var yScale = d3.scale.linear()
 		.range([height, 0]);
 		yScale.domain([0, d3.max(data) ]);
+	var barGroup;
+	function update(newData) {
 
-		var barWidth = width / data.length;
-
-		barChart = chart.selectAll("g").data(data);
-
-		barChart.selectAll('rect')
-		.attr("height", function(d) { return height - yScale(d); })
-		.attr("y", function(d) { return yScale(d); });
+		barGroup = chart.selectAll("g").data(newData);
 		
 
-		var bar = barChart.enter().append("g")
-		.attr("transform", function(d,i) { return "translate(" + (barWidth*i)+ ",0)"; });
 
-		bar.append("rect")
+		var barGroupEnter = barGroup.enter().append("g");
+
+		barGroupEnter.attr("transform", function(d,i) { console.log(arguments);  return "translate(" + (barWidth*i)+ ",0)"; });
+
+
+		barGroupEnter.append("rect")
 		.attr('fill',"green")
 		.attr("y", height)
 		.attr("height", function(d) { return height - yScale(d); })
 		.attr("width", barWidth-1)
+		barGroupEnter.append("text")
+
+		barGroup.select('rect')
 		.transition()
+		.attr("height", function(d) { return height - yScale(d); })
 		.attr("y", function(d) { return yScale(d); });
 
-		bar.append("text")
+		barGroup.select('text')
 		.attr("y", function(d) { return yScale(d) + 3; })
 		.attr("dy", ".75em")
 		.text(function(d) { return ""+d; });
-
-
-		barChart.exit()
-		.transition()
-		.duration(750)
-		.attr("y", 60)
-		.style("fill-opacity", 1e-6)
-		.remove();
 	}
 	update(data);
 
+
 	function highlight(toHighlight,color){
 		console.log(toHighlight);
-		barChart.select('rect').attr("fill",function(d,i) {return toHighlight.indexOf(i) > -1 ? color : "green" ;});
+		barGroup.select('rect').attr("fill",function(d,i) {return toHighlight.indexOf(i) > -1 ? color : "green" ;});
 	}
 
 	return {
