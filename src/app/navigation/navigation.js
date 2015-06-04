f7f84beba3cc213a183c0a6b406d8852342af2de
Maarten van Beek <maartenvanbeek@gmail.com>
2015-06-04 10:28:02 +0200

    Export button works, closes #36

---
+++ src/app/navigation/navigation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+/**
+ * @ngdoc overview
+ * @name narrator
+ * @description
+ * Narrator module. This module contains the narrator functionality i.e.
+ * viewing and editing narratives. 
+ */
+angular.module('navigation', []);
